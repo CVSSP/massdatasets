488a2fb49e050724e36a55d15a08f68d6bd7e663
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-06-28 12:51:40 +0100

    Create a installable module mass_datasets

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='mass_datasets',
+    version='0.1',
+    description='Datasets for musical audio source separation.',
+    author='Dominic Ward',
+    author_email='dw0031@surrey.ac.uk',
+    url='',
+    py_modules=['mass_datasets'],
+    long_description=long_description,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='audio music bss',
+    license='MIT',
+    install_requires=[
+        'pandas'
+    ]
+)
