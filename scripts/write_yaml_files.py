c97d5a219fdf2312f3e0b9185350c63dab217369
deeuu <deeuu@users.noreply.github.com>
2017-07-11 14:06:53 +0100

    Fixed examples

--- scripts/write_yaml_files.py
+++ scripts/write_yaml_files.py
@@ -1,9 +1,9 @@
-import mass_datasets
+import massdatasets
 
 
 if __name__ == '__main__':
 
-    ds = mass_datasets.DSD100('/vol/vssp/datasets/audio/DSD100')
+    ds = massdatasets.DSD100('/vol/vssp/datasets/audio/DSD100')
     ds.write('../data/DSD100')
-    ds = mass_datasets.MSD100('/vol/vssp/datasets/audio/MSD100')
+    ds = massdatasets.MSD100('/vol/vssp/datasets/audio/MSD100')
     ds.write('../data/MSD100')
