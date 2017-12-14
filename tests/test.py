dc9ed1dc7a710668183d8b8239a6c932c10b9df0
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-12-14 16:03:24 +0000

    Add test for join_dsd100_and_mus2016_dataframes()

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import massdatasets
+import massdatasets as massdata
 
 
 def test_mus2016():
@@ -8,7 +8,7 @@ def test_mus2016():
         '../data/sisec_mus_2017_full.csv'
     )
 
-    frame = massdatasets.Dataset.read('../data/MUS2016.yaml').to_pandas_df()
+    frame = massdata.Dataset.read('../data/MUS2016.yaml').to_pandas_df()
 
     frame = frame.sample(10)
 
@@ -20,3 +20,17 @@ def test_mus2016():
                             (ref_frame.target == row['source'])]
 
         assert(sub_ref['score'].values[0] == row['value'])
+
+
+def test_join_dsd100_and_mus2016_dataframes():
+
+    ref_cols = ['source', 'artist', 'audio_filepath', 'method', 'style',
+                'test_set', 'title', 'track_id', 'dataset', 'feature', 'value']
+
+    dsd_file = '../data/DSD100.yaml'
+    mus_file = '../data/MUS2016.yaml'
+    df_dsd = massdata.Dataset.read(dsd_file).to_pandas_df()
+    df_mus = massdata.Dataset.read(mus_file).to_pandas_df()
+    df = massdata.utilities.join_dsd100_and_mus2016_dataframes(df_dsd, df_mus)
+
+    assert(all(ref_cols == df.columns.values))
