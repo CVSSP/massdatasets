c97e70a29f722db4a405ecf6f13301ce8f8fc8d7
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2018-01-04 17:12:48 +0000

    No track_id in join_dsd100_and_mus2016_dataframes

--- massdatasets/utilities.py
+++ massdatasets/utilities.py
@@ -1,17 +1,11 @@
-import re
 import pandas as pd
 
 
 def join_dsd100_and_mus2016_dataframes(df_dsd, df_mus):
     "Combine DSD100 and MUS2016 pandas data frames"
 
-    # Add missing `method` and `track_id` to DSD100
+    # Add missing `method`
     df_dsd['method'] = 'ref'
-    df_dsd['track_id'] = 0
-    for idx, row in df_dsd.iterrows():
-        m = re.search('DSD100/(Sources|Mixtures)/(Dev|Test)/(\d{3})',
-                      row['audio_filepath'])
-        df_dsd.at[idx, 'track_id'] = int(m.group(3))
 
     # Remove songs from DSD100 not included in MUS2016
     track_ids = df_mus['track_id'].unique()
